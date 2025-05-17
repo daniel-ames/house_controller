91c64c879f128aec57fe9da10af06332a6760319
Daniel <37158683+daniel-ames@users.noreply.github.com>
2025-05-16 19:02:40 -0500

    should prolly add controller.h. duh

---
+++ controller.h
@@ -0,0 +1,17 @@
+#include <time.h>
+
+
+typedef struct sample_t {
+  float amps;
+  time_t timestamp;
+  int ordinal;
+  struct sample_t *next;
+} sample_t;
+
+typedef struct {
+  float average;
+  float max;
+  float min;
+  int samples;
+  __u_long duration;
+} summary_t;
