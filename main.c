f2b39952740b5c50d38eca24c007cf3277df3e23
Daniel <37158683+daniel-ames@users.noreply.github.com>
2025-05-24 15:22:00 -0500

    remove sigpipe handler since I ditched esmtp

--- main.c
+++ main.c
@@ -34,12 +34,6 @@ void handle_sig(int sig)
     exit(0);
 }
 
-void handle_sigpipe()
-{
-    // ignore it
-    return;
-}
-
 // make the time look like: 3:45:24 PM
 void time_my_way(struct tm * time, char * out)
 {
@@ -202,7 +196,6 @@ int main ()
     // close the port cleanly when I ctrl+C this sumbitch
     signal(SIGINT, handle_sig);
     signal(SIGTERM, handle_sig);
-    signal(SIGPIPE, handle_sigpipe);
 
     ostream = stdout;
 
