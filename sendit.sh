2365e314acafae52579f9f41019f2ba80039cbe7
Daniel <37158683+daniel-ames@users.noreply.github.com>
2025-05-27 16:39:49 -0500

    Change 'seconds' to 'samples'. Lose the graph title. Silence unix2dos output to stderr. Print a dot for every sample received from esp

--- sendit.sh
+++ sendit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## create the graph
-gnuplot -e "set terminal png; set output 'out.png'; set yrange [0:20]; set xlabel 'seconds'; set ylabel 'amps'; set title ''; plot 'plots.dat' with lines"
+gnuplot -e "set terminal png; set output 'out.png'; set yrange [0:20]; set xlabel 'samples'; set ylabel 'amps'; plot 'plots.dat' with lines notitle"
 
 ## there should now be an out.png with our graph. b64 it
 base64 out.png > out.b64
@@ -14,6 +14,6 @@ echo "--xxxx38th parallel--">>email
 echo >>email
 
 ## line enings must be \r\n for smtp
-unix2dos email
+unix2dos email 2>/dev/null
 
 sendmail -t < email
