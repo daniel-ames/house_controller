6f2894032a97cdb23e935fe921027d09a46ef791
dan <daniel.ames@hp.com>
2026-03-01 14:13:00 -0600

    Simplify the node-walking logic by making it more complicated

--- scheduler.h
+++ scheduler.h
@@ -3,8 +3,8 @@ typedef void (*handler_callback_t)(void *ctx);
 
 typedef struct session {
   uint32_t id;
-  uint32_t inactivity_timeout;
-  uint32_t last_activity_time;
+  uint64_t inactivity_timeout;
+  uint64_t last_activity_time;
   bool abort_session;
   bool end_session;
   void *ctx;   // Context TBD
@@ -14,6 +14,6 @@ typedef struct session {
 
 
 void* scheduler_thread(void *ptr);
-uint32_t create_session(uint32_t inactivity_time, handler_callback_t);
-void pet_the_dog(uint32_t session_id);
+uint32_t create_session(uint32_t inactivity_time, handler_callback_t, void *ctx);
+bool pet_the_dog(uint32_t session_id);
 
