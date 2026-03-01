722c5289a30e1b6da865e9f7254c5315e173a9cd
Daniel <37158683+daniel-ames@users.noreply.github.com>
2026-03-01 11:46:47 -0600

    Use mutexes. Queue callbacks inside the lock, and dequeue outside the lock

--- scheduler.c
+++ scheduler.c
@@ -17,31 +17,24 @@
 #include "scheduler.h"
 #include "logger.h"
 
+typedef struct callback_list {
+  handler_callback_t callback;
+  void *ctx;
+  struct callback_list *next;
+} callback_list_t;
+
 static uint32_t session_id_ctr = 0;
-// static volatile bool list_is_locked = false;
 static pthread_mutex_t list_lock_m = PTHREAD_MUTEX_INITIALIZER;
-
 static session_t *sessions = NULL;
-
-// static inline void get_list_lock()
-// {
-//   while(list_is_locked);
-//   list_is_locked = true;
-// }
-// static inline void release_list_lock()
-// {
-//   list_is_locked = false;
-// }
+static callback_list_t *callbacks = NULL;
 
 
-uint32_t create_session(uint32_t inactivity_timeout, void (*callback)(session_t*))
+uint32_t create_session(uint32_t inactivity_timeout, handler_callback_t callback)
 {
   session_t *s;
   session_t *new_session = malloc(sizeof(session_t));
   struct timespec ts;
   clock_gettime(CLOCK_MONOTONIC, &ts);
-
-  pthread_mutex_lock(&list_lock_m);
 
   new_session->id = session_id_ctr++;
   new_session->inactivity_timeout = inactivity_timeout;
@@ -51,6 +44,7 @@ uint32_t create_session(uint32_t inactivity_timeout, void (*callback)(session_t*
   new_session->callback = callback;
   new_session->next = NULL;
 
+  pthread_mutex_lock(&list_lock_m);
   // scheduler is paused
   if(!sessions) {
     sessions = new_session;
@@ -118,11 +112,40 @@ static session_t* remove_this_session_and_get_the_next_one(uint32_t session_id)
   return new_next;
 }
 
+static void push_cb(callback_list_t cb_in)
+{
+  callback_list_t *cbl = malloc(sizeof(*callbacks));
+  static callback_list_t *prev = NULL;
+
+  memcpy(cbl, &cb_in, sizeof(cb_in));
+  cbl->next = NULL;
+  if (!callbacks)
+    // this is the first one
+    callbacks = cbl;
+  else
+    // this is NOT the first one, so point the previous to this one
+    prev->next = cbl;
+
+  prev = cbl;
+}
+
+static callback_list_t pop_cb()
+{
+  callback_list_t cbl = {NULL, NULL, NULL};
+  if (!callbacks) return cbl;
+  cbl = *callbacks;
+  callback_list_t *cbl_next = callbacks->next;
+  free(callbacks);
+  callbacks = cbl_next;
+  return cbl;
+}
+
 void* scheduler_thread(void *ptr)
 {
   session_t *s;
   struct timespec ts;
   uint64_t current_time = 0;
+  callback_list_t cbl;
 
   // This is a busy loop that manages sessions
   while(1) {
@@ -142,12 +165,10 @@ void* scheduler_thread(void *ptr)
           continue;
         }
         if(s->end_session) {
-          // Fire the callback. These callbacks must be ridiculously fast.
-          // If they need to do more than a few instructions (and they always will),
-          // then a callback should spawn a new thread to do the work, and return fast.
-          // The scheduler is for scheduling, not sending email alerts.
-          // TODO: instead of calling the callback, should we just spawn a thread for it here?
-          (s->callback)(s);
+          // Queue the callback
+          cbl.callback = s->callback;
+          cbl.ctx = s->ctx;
+          push_cb(cbl);
           // This session is done. Remove it.
           s = remove_this_session_and_get_the_next_one(s->id);
           if(!s) break;
@@ -160,6 +181,17 @@ void* scheduler_thread(void *ptr)
         s = s->next;
       } while(s);
       pthread_mutex_unlock(&list_lock_m);
+
+      // Dequeue the callbacks. These callbacks must be ridiculously fast.
+      // If they need to do more than a few instructions (and they always will),
+      // then a callback should spawn a new thread to do the work, and return fast.
+      // The scheduler is for scheduling, not sending email alerts.
+      // TODO: instead of calling the callback, should we just spawn a thread for it here?
+      cbl = pop_cb();
+      while(cbl.callback) {
+        cbl.callback(cbl.ctx);
+        cbl = pop_cb();
+      }
     }
 
     // give list modifiers a generous chance to make changes
--- scheduler.h
+++ scheduler.h
@@ -1,5 +1,5 @@
 
-
+typedef void (*handler_callback_t)(void *ctx);
 
 typedef struct session {
   uint32_t id;
@@ -7,12 +7,13 @@ typedef struct session {
   uint32_t last_activity_time;
   bool abort_session;
   bool end_session;
-  void (*callback)(struct session *s);
+  void *ctx;   // Context TBD
+  handler_callback_t callback;
   struct session *next;
 } session_t;
 
 
 void* scheduler_thread(void *ptr);
-uint32_t create_session(uint32_t inactivity_time, void (*callback)(session_t*));
+uint32_t create_session(uint32_t inactivity_time, handler_callback_t);
 void pet_the_dog(uint32_t session_id);
 
