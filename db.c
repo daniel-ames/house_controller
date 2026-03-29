09344241b66e2bf87015399b7836ac18421c3d09
dan <daniel.ames@hp.com>
2026-03-29 13:15:21 -0500

    change precision to ns since I'm now sending ns wall time with each sample

--- db.c
+++ db.c
@@ -11,7 +11,7 @@ void write_to_db(char *influxdb_line_protocol)
   const char *api_token = "nHh-S1f5Wlz_4LvsS0q9sBxsFR3u5UsTEnZDEgXuTn4JWn-8VESGJ-aW5yvYRY4vEXWb8ApZFW6qA9G3dicpWA==";
 
   char url[512];
-  snprintf(url, sizeof(url), "%s/api/v2/write?org=%s&bucket=%s&precision=s", db_url, org, bucket);
+  snprintf(url, sizeof(url), "%s/api/v2/write?org=%s&bucket=%s&precision=ns", db_url, org, bucket);
 
   CURL *curl = curl_easy_init();
   if (!curl) {
